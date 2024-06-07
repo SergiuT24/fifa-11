5c72c31319ed3a6dc16b1b37745f9e29a7dfd966
Sergiu Tudos <cambino7777@gmail.com>
2024-06-07 17:10:30 +0300

    latest score foto

---
+++ src/components/Menu/Posts/Post/LatestScores/MatchDetails.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const MatchDetails = () => {
+	const [isFullSize, setIsFullSize] = useState(false);
+
+	const handleImageClick = () => {
+		setIsFullSize(true);
+	};
+
+	const handleCloseClick = () => {
+		setIsFullSize(false);
+	};
+
+	return (
+		<div className={`match-details flex justify-center w-full ${isFullSize ? 'full-size' : ''}`}>
+			<div className='w-96'>
+				{isFullSize && (
+					<button className="close-button" onClick={handleCloseClick}>
+						&times;
+					</button>
+				)}
+				<img
+					src="./match-details-img/1.png"
+					alt="details"
+					onClick={handleImageClick}
+					className={isFullSize ? 'expanded' : ''}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default MatchDetails;
