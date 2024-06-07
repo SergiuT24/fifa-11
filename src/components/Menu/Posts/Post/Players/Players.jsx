c4a779a17b5f35bd829ddd49fca4c8c8fc132126
Sergiu Tudos <cambino7777@gmail.com>
2024-06-07 22:54:57 +0300

    jsnick perfect

--- src/components/Menu/Posts/Post/Players/Players.jsx
+++ src/components/Menu/Posts/Post/Players/Players.jsx
@@ -7,7 +7,7 @@ const Players = () => {
 			<PlayersTeam team={'TEAM 1'} playerOne={'Anna'} imgOne={'./players-img/Anna.png'} playerTwo={'DartRin'} imgTwo={'./players-img/Rin.png'} />
 			<PlayersTeam team={'TEAM 2'} playerOne={'Box'} imgOne={'./players-img/Box.png'} playerTwo={'14 France'} imgTwo={'./players-img/France.png'} />
 			<PlayersTeam team={'TEAM 3'} playerOne={'Wenger'} imgOne={'./players-img/Wenger.png'} playerTwo={'Petr-Pirat'} imgTwo={'./players-img/Petr.png'} />
-			<PlayersTeam team={'TEAM 4'} playerOne={'none'} playerTwo={'none'} />
+			<PlayersTeam team={'TEAM 4'} playerOne={'Jsnick'} playerTwo={'Perfect'} imgOne={'./players-img/Jsnick.png'} imgTwo={'./players-img/Perfect.png'} />
 			<PlayersTeam team={'TEAM 5'} playerOne={'none'} playerTwo={'none'} />
 			<PlayersTeam team={'TEAM 6'} playerOne={'none'} playerTwo={'none'} />
 		</div>
