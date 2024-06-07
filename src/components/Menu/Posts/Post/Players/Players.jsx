c4f03cfb08bae42a90285b3f7875bc73620b6b98
Sergiu Tudos <cambino7777@gmail.com>
2024-06-07 21:37:57 +0300

    wenger-petr

--- src/components/Menu/Posts/Post/Players/Players.jsx
+++ src/components/Menu/Posts/Post/Players/Players.jsx
@@ -6,7 +6,7 @@ const Players = () => {
 		<div className='flex gap-11'>
 			<PlayersTeam team={'TEAM 1'} playerOne={'Anna'} imgOne={'./players-img/Anna.png'} playerTwo={'DartRin'} imgTwo={'./players-img/Rin.png'} />
 			<PlayersTeam team={'TEAM 2'} playerOne={'Box'} imgOne={'./players-img/Box.png'} playerTwo={'14 France'} imgTwo={'./players-img/France.png'} />
-			<PlayersTeam team={'TEAM 3'} playerOne={'none'} playerTwo={'none'} />
+			<PlayersTeam team={'TEAM 3'} playerOne={'Wenger'} imgOne={'./players-img/Wenger.png'} playerTwo={'Petr-Pirat'} imgTwo={'./players-img/Petr.png'} />
 			<PlayersTeam team={'TEAM 4'} playerOne={'none'} playerTwo={'none'} />
 			<PlayersTeam team={'TEAM 5'} playerOne={'none'} playerTwo={'none'} />
 			<PlayersTeam team={'TEAM 6'} playerOne={'none'} playerTwo={'none'} />
