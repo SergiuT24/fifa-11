917a41705bf44082cd293773b5a78bbef6d65819
Sergiu Tudos <cambino7777@gmail.com>
2024-06-10 02:01:40 +0300

    quarterfinals

--- src/components/Posts/Main/PlayOffs/PlayOffs.jsx
+++ src/components/Posts/Main/PlayOffs/PlayOffs.jsx
@@ -7,36 +7,25 @@ const PlayOffs = () => {
 		<div className='p-14'>
 			<div className='flex justify-around'>
 				<div className='flex flex-col gap-8 relative'>
-					<PlayOffTitle value={'semi-final'} />
-					<div className='line-one border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					<PlayOffTitle value={'1/4 Quarter-Finals'} />
+					<div className='line-quarterone border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 2 Group B'} score={'?'} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 3 Group A'} score={'?'} />
 					</div>
-					<div className='line-two border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-					</div>
-				</div>
-				<div className='flex flex-col gap-8 relative'>
-					<PlayOffTitle value={'semi-final'} />
-					<div className='line-one border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-					</div>
-					<div className='line-two border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					<div className='line-quarter border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 2 Group A'} score={'?'} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 3 Group B'} score={'?'} />
 					</div>
 				</div>
 				<div className='flex flex-col gap-8 relative'>
-					<PlayOffTitle value={'semi-final'} />
+					<PlayOffTitle value={'1/2 semi-final'} />
 					<div className='line-one border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 1 Group A'} score={'?'} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={'?'} />
 					</div>
 					<div className='line-two border rounded-lg p-3'>
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
-						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Place 1 Group B'} score={'?'} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={'?'} />
 					</div>
 				</div>
 				<div className='flex flex-col gap-8 relative'>
