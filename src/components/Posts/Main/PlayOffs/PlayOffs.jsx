6c9210e3eaa76d0afe456a0d6e75fd772e1cbadd
Sergiu Tudos <cambino7777@gmail.com>
2024-06-10 01:24:31 +0300

    malemeria add 2

--- src/components/Posts/Main/PlayOffs/PlayOffs.jsx
+++ src/components/Posts/Main/PlayOffs/PlayOffs.jsx
@@ -18,6 +18,28 @@ const PlayOffs = () => {
 					</div>
 				</div>
 				<div className='flex flex-col gap-8 relative'>
+					<PlayOffTitle value={'semi-final'} />
+					<div className='line-one border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					</div>
+					<div className='line-two border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					</div>
+				</div>
+				<div className='flex flex-col gap-8 relative'>
+					<PlayOffTitle value={'semi-final'} />
+					<div className='line-one border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					</div>
+					<div className='line-two border rounded-lg p-3'>
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
+					</div>
+				</div>
+				<div className='flex flex-col gap-8 relative'>
 					<PlayOffTitle value={'final'} />
 					<div className='line-tree border rounded-lg p-3 mt-11'>
 						<PlayOffBracket img={<img src="https://static.flashscore.com/res/image/data/CKvcQ6zS-vZG58BBc.png" />} team={'Team-Name'} score={0} />
