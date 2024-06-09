2efcef85adf320e14d186f4b310ff10baf8faca2
Sergiu Tudos <cambino7777@gmail.com>
2024-06-10 02:07:55 +0300

    start world cup

--- src/components/Posts/Post/PostTitle.jsx
+++ src/components/Posts/Post/PostTitle.jsx
@@ -5,7 +5,7 @@ const PostTitle = () => {
 		<div className='title mb-4'>
 			<div className='flex justify-center gap-3 items-center pt-4'>
 				<img src='./cups-img/world-cup.png' className=' w-14' />
-				<h1 className='text-2xl uppercase text-white text-center'>FIFA 11 <span className=' text-red-600'>Word Cup</span> 2024</h1>
+				<h1 className='text-2xl uppercase text-white text-center'>FIFA 11 <span className=' text-red-600'>Word Cup</span> 2024 Start June 10 2024 12:00</h1>
 			</div>
 			<h2 className='text-xl pl-1 pt-4 text-white pb-4 text-center'>Game Mode: 2x2</h2>
 		</div>
