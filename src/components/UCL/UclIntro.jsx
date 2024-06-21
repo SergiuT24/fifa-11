0abeb9b90d4ef1fabca51d7285dcf28240e0e178
Sergiu Tudos <cambino7777@gmail.com>
2024-06-21 16:22:00 +0300

    video fix link

--- src/components/UCL/UclIntro.jsx
+++ src/components/UCL/UclIntro.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const UclIntro = () => {
+	const videoRef = useRef(null);
+
+	useEffect(() => {
+		if (videoRef.current) {
+			videoRef.current.play().catch(error => {
+				console.error('Error attempting to play video:', error);
+			});
+		}
+	}, []);
+
 	return (
 		<div className='flex justify-evenly'>
 			<div className='flex justify-start'>
 				<img src="/img/background/cr7.png" alt="Background" className='h-96' />
 			</div>
 			<div className='flex justify-center'>
-				<video width="600" controls autoPlay>
+				<video ref={videoRef} width="600" controls autoPlay>
 					<source src="/videos/ucl-intro.mp4" type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
@@ -17,3 +27,4 @@ const UclIntro = () => {
 }
 
 export default UclIntro;
+
