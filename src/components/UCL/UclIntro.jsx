b8809f00e3017afe9fd5d1e2a3bd25d2d09f7f65
Sergiu Tudos <cambino7777@gmail.com>
2024-06-21 16:29:28 +0300

    video intro prime version

--- src/components/UCL/UclIntro.jsx
+++ src/components/UCL/UclIntro.jsx
@@ -1,30 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
 const UclIntro = () => {
-	const videoRef = useRef(null);
-	const [isVideoStarted, setIsVideoStarted] = useState(false);
-
-	const handlePlayVideo = () => {
-		if (videoRef.current) {
-			videoRef.current.play().catch(error => {
-				console.error('Error attempting to play video:', error);
-			});
-			setIsVideoStarted(true);
-		}
-	};
-
 	return (
 		<div className='flex justify-evenly'>
 			<div className='flex justify-start'>
 				<img src="/img/background/cr7.png" alt="Background" className='h-96' />
 			</div>
 			<div className='flex justify-center'>
-				{!isVideoStarted && (
-					<button onClick={handlePlayVideo} className='bg-blue-500 text-white px-4 py-2 rounded'>
-						Play Video
-					</button>
-				)}
-				<video ref={videoRef} width="600" controls={!isVideoStarted}>
+				<video width="600" controls autoPlay>
 					<source src="/videos/ucl-intro.mp4" type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
