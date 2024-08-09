7919a01f256b96292d1044cb66774c92c97f191b
Sergiu Tudos <cambino7777@gmail.com>
2024-08-09 14:03:12 +0300

    aug 9 14:00

--- src/data/ProLeagueInitialDataPlayersStatsSeasonOne1x1.jsx
+++ src/data/ProLeagueInitialDataPlayersStatsSeasonOne1x1.jsx
@@ -10,14 +10,14 @@ export const initialPlayers = [
 ];
 
 export const playerStats = [
-	{ id: 'mysterio', goals: 9, matchesPlayed: 4, assists: 0 },
-	{ id: 'D.Drogba', goals: 8, matchesPlayed: 4, assists: 0 },
-	{ id: 'Berserk', goals: 32, matchesPlayed: 10, assists: 0 },
+	{ id: 'mysterio', goals: 9, matchesPlayed: 6, assists: 0 },
+	{ id: 'D.Drogba', goals: 16, matchesPlayed: 8, assists: 0 },
+	{ id: 'Berserk', goals: 39, matchesPlayed: 12, assists: 0 },
 	{ id: 'NSKRaven', goals: 4, matchesPlayed: 4, assists: 0 },
 	{ id: 'TOTO', goals: 5, matchesPlayed: 4, assists: 0 },
-	{ id: 'Anna', goals: 28, matchesPlayed: 6, assists: 0 },
+	{ id: 'Anna', goals: 41, matchesPlayed: 12, assists: 0 },
+	{ id: 'HAM', goals: 15, matchesPlayed: 6, assists: 0 },
 	// { id: 'A.Wenger', goals: , matchesPlayed: 2, assists: 0 },
-	// { id: 'HAM', goals: , matchesPlayed: 2, assists: 0 },
 	// { id: '', goals: 0, matchesPlayed: 2, assists: 0 },
 	// { id: '', goals: 0, matchesPlayed: 2, assists: 0 },
 ];
