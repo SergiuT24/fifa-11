c298c02f23bacfab12a49eb3246b521b33d4c0bf
Sergiu Tudos <cambino7777@gmail.com>
2024-09-10 16:15:00 +0300

    sep 10 update 16:15

--- src/data/seasonTwo1x1/ProLeagueTableResults.jsx
+++ src/data/seasonTwo1x1/ProLeagueTableResults.jsx
@@ -6,6 +6,10 @@ export const initialTeams = [
 	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
 export const matchResults = [
--- src/pages/tournaments/1x1/season2/SquadsForSeason.jsx
+++ src/pages/tournaments/1x1/season2/SquadsForSeason.jsx
@@ -6,43 +6,53 @@ const SquadsForSeason = () => {
 		<div className='flex flex-col gap-5 ml-4'>
 			<PlayersTeam
 				team={'TEAM 1'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'Jsnick'}
+				imgOne={'/img/players/jsnick.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 2'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'Berserk'}
+				imgOne={'/img/players/berserk.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 3'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'mysterio'}
+				imgOne={'/img/players/mysterio.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 4'}
-				playerOne={'empty'}
+				playerOne={'HAM'}
 				imgOne={'/img/players/avatar.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 5'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'D.Drogba'}
+				imgOne={'/img/players/d-drogba.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 6'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'DartRin'}
+				imgOne={'/img/players/rin.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 7'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'Anna'}
+				imgOne={'/img/players/anna.png'}
 			/>
 			<PlayersTeam
 				team={'TEAM 8'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
+				playerOne={'TOTO'}
+				imgOne={'/img/players/toto.png'}
+			/>
+			<PlayersTeam
+				team={'TEAM 9'}
+				playerOne={'M@KSON'}
+				imgOne={'/img/players/makson.png'}
+			/>
+			<PlayersTeam
+				team={'TEAM 10'}
+				playerOne={'Nikita'}
+				imgOne={'/img/players/nikita.png'}
 			/>
 		</div>
 	);
