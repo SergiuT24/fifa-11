5b7cf32a0d1a78c16308559284716310722bccee
Sergiu Tudos <cambino7777@gmail.com>
2024-09-12 19:39:10 +0300

    update sep 12 19:40

--- src/data/seasonTwo1x1/ProLeaguePlayerStats.jsx
+++ src/data/seasonTwo1x1/ProLeaguePlayerStats.jsx
@@ -1,17 +1,25 @@
 export const initialPlayers = [
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
-	{ id: 'empty', team: 'empty', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'Berserk', team: 'Bayern Munich', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'Jsnick', team: 'AC Milan', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'Anna', team: 'Lyon', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'M@KSON', team: 'Arsenal', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'D.Drogba', team: 'Barcelona', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'mysterio', team: 'Liverpool', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'HAM', team: 'Chelsea', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'Nikita', team: 'Real Madrid', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'TOTO', team: 'Atletico Madrid', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
+	{ id: 'DartRin', team: 'Manchester United', goals: 0, matchesPlayed: 0, assists: 0, playerNote: 0 },
 ];
 
 export const playerStats = [
+	{ id: 'Berserk', goals: 45, matchesPlayed: 14, assists: 0 },
+	{ id: 'Jsnick', goals: 38, matchesPlayed: 14, assists: 0 },
+	{ id: 'Anna', goals: 65, matchesPlayed: 18, assists: 0 },
+	{ id: 'M@KSON', goals: 31, matchesPlayed: 18, assists: 0 },
+	{ id: 'D.Drogba', goals: 30, matchesPlayed: 16, assists: 0 },
+	{ id: 'mysterio', goals: 30, matchesPlayed: 16, assists: 0 },
+	{ id: 'HAM', goals: 27, matchesPlayed: 12, assists: 0 },
+	{ id: 'Nikita', goals: 19, matchesPlayed: 16, assists: 0 },
+	{ id: 'TOTO', goals: 15, matchesPlayed: 12, assists: 0 },
+	{ id: 'DartRin', goals: 29, matchesPlayed: 16, assists: 0 }
 ];
