1ca40c85b8c96f0928d7d0a8a4f9a8a7d2564d4a
Sergiu Tudos <cambino7777@gmail.com>
2024-09-13 23:56:04 +0300

    update 23:55 sep 13

--- src/pages/tournaments/1x1/season2/kingscup/KingsCupPlayOffs.jsx
+++ src/pages/tournaments/1x1/season2/kingscup/KingsCupPlayOffs.jsx
@@ -5,10 +5,10 @@ const KingsCupPlayOffs = () => {
 	return (
 		<div className='py-16 darkblue'>
 			<div className='flex flex-col gap-10'>
-				<h2 className='text-center text-orange-500'>1/8-Finals</h2>
+				<h2 className='text-center text-orange-500'>League Stage</h2>
 				<div className='flex gap-14 justify-center'>
 					<Square
-						teamOne={'5 place'}
+						teamOne={'10 place'}
 						teamTwo={'8 place'}
 						scoreFirstTeamOne={'?'}
 						scoreSecondTeamOne={'?'}
@@ -16,8 +16,29 @@ const KingsCupPlayOffs = () => {
 						scoreSecondTeamTwo={'?'}
 					/>
 					<Square
-						teamOne={'6 place'}
+						teamOne={'9 place'}
 						teamTwo={'7 place'}
+						scoreFirstTeamOne={'?'}
+						scoreSecondTeamOne={'?'}
+						scoreFirstTeamTwo={'?'}
+						scoreSecondTeamTwo={'?'}
+					/>
+				</div>
+			</div>
+			<div className='flex flex-col gap-10 mt-9'>
+				<h2 className='text-center text-orange-500'>1/8-Finals</h2>
+				<div className='flex gap-14 justify-center'>
+					<Square
+						teamOne={'5 place'}
+						teamTwo={'empty'}
+						scoreFirstTeamOne={'?'}
+						scoreSecondTeamOne={'?'}
+						scoreFirstTeamTwo={'?'}
+						scoreSecondTeamTwo={'?'}
+					/>
+					<Square
+						teamOne={'6 place'}
+						teamTwo={'empty'}
 						scoreFirstTeamOne={'?'}
 						scoreSecondTeamOne={'?'}
 						scoreFirstTeamTwo={'?'}
