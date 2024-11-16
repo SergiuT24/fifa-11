6d2bcbfddc2e3feb0e3f3b83933f118e208fca01
Sergiu Tudos <cambino7777@gmail.com>
2024-11-16 15:20:29 +0200

    nov 16 15:20

--- src/data/seasonFive1x1/results.js
+++ src/data/seasonFive1x1/results.js
@@ -1,16 +1,12 @@
 export const fetchResults = async () => {
+	console.log('Fetching results...');
 	try {
 		const response = await fetch('https://match-results-bucket.s3.eu-central-1.amazonaws.com/matchResults.json');
-		const data = await response.json();
+		console.log('Response received:', response);
 
-		// Prelucrarea datelor și returnarea lor într-un format dorit
-		return data.map((match, index) => ({
-			id: (index + 1).toString(),
-			teamOne: match.teamOne,
-			scoreOne: match.scoreOne,
-			teamTwo: match.teamTwo,
-			scoreTwo: match.scoreTwo
-		}));
+		const data = await response.json();
+		console.log('Data parsed:', data);
+		return data;
 	} catch (error) {
 		console.error('Error fetching match results:', error);
 		return [];
