53d90ae0b1a799b2d4440e0ab78eb42a005b4f9c
Sergiu Tudos <cambino7777@gmail.com>
2024-12-02 19:37:47 +0200

    new tournament update dec 2 19:37

--- src/data/seasonSeven1x1/ProLeagueTableResults.jsx
+++ src/data/seasonSeven1x1/ProLeagueTableResults.jsx
@@ -1,61 +1,61 @@
+import Psv from '../../components/league/4star-teams/Psv';
+import Fenerbahce from '../../components/league/4star-teams/Fenerbahce';
+import WestHam from '../../components/league/4star-teams/WestHam';
+import Psg from '../../components/league/4star-teams/Psg';
 import Galatasaray from '../../components/league/4star-teams/Galatasaray';
-import Cska from '../../components/league/4star-teams/Cska';
-import Fiorentina from '../../components/league/4star-teams/Fiorentina';
-import Schalke from '../../components/league/4star-teams/Schalke';
-import Zenit from '../../components/league/4star-teams/Zenit';
+import Werder from '../../components/league/4star-teams/Werder';
+import Stuttgart from '../../components/league/4star-teams/Stuttgart';
+import Palermo from '../../components/league/4star-teams/Palermo';
+import Sporting from '../../components/league/4star-teams/Sporting';
+import FulHam from '../../components/league/4star-teams/FulHam';
+import Wolfsburg from '../../components/league/4star-teams/Wolfsburg';
 import Napoli from '../../components/league/4star-teams/Napoli';
-import Ajax from '../../components/league/4star-teams/Ajax';
-import Genoa from '../../components/league/4star-teams/Genoa';
+
 
 export const initialTeams = [
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Palermo', name: <Palermo />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Sporting', name: <Sporting />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'PSV', name: <Psv />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Stuttgart', name: <Stuttgart />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'FulHam', name: <FulHam />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Galatasaray', name: <Galatasaray />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
 export const additionalTeams = [
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'empty', name: 'empty', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Fenerbahce', name: <Fenerbahce />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'WestHam', name: <WestHam />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'PSG', name: <Psg />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Werder', name: <Werder />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Wolfsburg', name: <Wolfsburg />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Napoli', name: <Napoli />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
 
-export const matchResults = [
-	{ id: '1', teamOne: 'Napoli', scoreOne: 1, teamTwo: 'CSKA', scoreTwo: 2 },
-	{ id: '2', teamOne: 'CSKA', scoreOne: 2, teamTwo: 'Napoli', scoreTwo: 3 },
-	{ id: '1', teamOne: 'Genoa', scoreOne: 1, teamTwo: 'Ajax', scoreTwo: 2 },
-	{ id: '2', teamOne: 'Ajax', scoreOne: 2, teamTwo: 'Genoa', scoreTwo: 3 },
-];
+export const matchResults = [];
 
-// const fetchResults = async () => {
-// 	console.log('Fetching results...');
-// 	try {
-// 		const response = await fetch(`https://match-results-bucket.s3.eu-central-1.amazonaws.com/matchResults.json?timestamp=${Date.now()}`);
-// 		const data = await response.json();
+const fetchResults = async () => {
+	console.log('Fetching results...');
+	try {
+		const response = await fetch(`https://match-results-bucket.s3.eu-central-1.amazonaws.com/matchResults.json?timestamp=${Date.now()}`);
+		const data = await response.json();
 
-// 		matchResults.length = 0;
-// 		data.forEach(item => {
-// 			matchResults.push({
-// 				id: item.id,
-// 				teamOne: item.teamOne,
-// 				scoreOne: item.scoreOne,
-// 				teamTwo: item.teamTwo,
-// 				scoreTwo: item.scoreTwo
-// 			});
-// 		});
+		matchResults.length = 0;
+		data.forEach(item => {
+			matchResults.push({
+				id: item.id,
+				teamOne: item.teamOne,
+				scoreOne: item.scoreOne,
+				teamTwo: item.teamTwo,
+				scoreTwo: item.scoreTwo
+			});
+		});
 
-// 		return matchResults;
-// 	} catch (error) {
-// 		console.error('Error fetching match results:', error);
-// 		return [];
-// 	}
-// };
+		return matchResults;
+	} catch (error) {
+		console.error('Error fetching match results:', error);
+		return [];
+	}
+};
 
-// fetchResults().then(() => {
-// });
+fetchResults().then(() => {
+});
--- src/pages/tournaments/1x1/season7/SquadsForSeason.jsx
+++ src/pages/tournaments/1x1/season7/SquadsForSeason.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import PlayersTeam from '../../../../components/league/players/PlayersTeam';
-// import Galatasaray from '../../../../components/league/4star-teams/Galatasaray';
+import Psv from '../../../../components/league/4star-teams/Psv';
+import Fenerbahce from '../../../../components/league/4star-teams/Fenerbahce';
+import WestHam from '../../../../components/league/4star-teams/WestHam';
+import Psg from '../../../../components/league/4star-teams/Psg';
+import Galatasaray from '../../../../components/league/4star-teams/Galatasaray';
+import Werder from '../../../../components/league/4star-teams/Werder';
+import Stuttgart from '../../../../components/league/4star-teams/Stuttgart';
+import Palermo from '../../../../components/league/4star-teams/Palermo';
+import Sporting from '../../../../components/league/4star-teams/Sporting';
+import FulHam from '../../../../components/league/4star-teams/FulHam';
+import Wolfsburg from '../../../../components/league/4star-teams/Wolfsburg';
+import Napoli from '../../../../components/league/4star-teams/Napoli';
+
 
 const SquadsForSeason = () => {
 	return (
@@ -9,62 +21,62 @@ const SquadsForSeason = () => {
 				team={'TEAM 1'}
 				playerOne={'Dagestan'}
 				imgOne={'/img/players/dagestan.png'}
-				club={'empty'} />
+				club={<Psv />} />
 			<PlayersTeam
 				team={'TEAM 2'}
 				playerOne={'VITALY'}
 				imgOne={'/img/players/vitaly.png'}
-				club={'empty'} />
+				club={<Fenerbahce />} />
 			<PlayersTeam
 				team={'TEAM 3'}
 				playerOne={'TOTO'}
 				imgOne={'/img/players/toto.png'}
-				club={'empty'} />
+				club={<WestHam />} />
 			<PlayersTeam
 				team={'TEAM 4'}
 				playerOne={'Nikita'}
 				imgOne={'/img/players/nikita.png'}
-				club={'empty'} />
+				club={<Psg />} />
 			<PlayersTeam
 				team={'TEAM 5'}
 				playerOne={'M@KSON'}
 				imgOne={'/img/players/makson.png'}
-				club={'empty'} />
+				club={<Galatasaray />} />
 			<PlayersTeam
 				team={'TEAM 6'}
 				playerOne={'Rayon 13'}
 				imgOne={'/img/players/rayon13.png'}
-				club={'empty'} />
+				club={<Werder />} />
 			<PlayersTeam
 				team={'TEAM 7'}
 				playerOne={'Berserk'}
 				imgOne={'/img/players/berserk.png'}
-				club={'empty'} />
+				club={<Stuttgart />} />
 			<PlayersTeam
 				team={'TEAM 8'}
 				playerOne={'HAM'}
 				imgOne={'/img/players/avatar.png'}
-				club={'empty'} />
+				club={<Palermo />} />
 			<PlayersTeam
 				team={'TEAM 9'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
-				club={'empty'} />
+				playerOne={'Jsnick'}
+				imgOne={'/img/players/jsnick.png'}
+				club={<FulHam />} />
 			<PlayersTeam
 				team={'TEAM 10'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
-				club={'empty'} />
+				playerOne={'A.Wenger'}
+				imgOne={'/img/players/wenger.png'}
+				club={<Napoli />} />
 			<PlayersTeam
 				team={'TEAM 11'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
-				club={'empty'} />
+				playerOne={'Anna'}
+				imgOne={'/img/players/anna.png'}
+				club={<Sporting />} />
 			<PlayersTeam
 				team={'TEAM 12'}
-				playerOne={'empty'}
-				imgOne={'/img/players/avatar.png'}
-				club={'empty'} />
+				playerOne={'Igvasalek'}
+				imgOne={'/img/players/igvasalek.png'}
+				club={<Wolfsburg />} />
 		</div>
 	);
 }
