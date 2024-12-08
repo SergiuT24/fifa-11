2d7f03ac82742507b3b6011c8386b7c41340ad8a
Sergiu Tudos <cambino7777@gmail.com>
2024-12-08 18:39:34 +0200

    update dec 8 18:40

--- src/pages/tournaments/1x1/season7/europeleague/EuropeLeaguePlayOffs.jsx
+++ src/pages/tournaments/1x1/season7/europeleague/EuropeLeaguePlayOffs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Square from '../../../../../components/league/square/Square';
-import Fiorentina from '../../../../../components/league/4star-teams/Fiorentina';
+import Sporting from '../../../../../components/league/4star-teams/Sporting';
+import Galatasaray from '../../../../../components/league/4star-teams/Galatasaray';
 
 const EuropeLeaguePlayOffs = () => {
 	return (
@@ -10,14 +11,14 @@ const EuropeLeaguePlayOffs = () => {
 				<div className='flex gap-14 justify-center'>
 					<Square
 						teamOne={'Group A 1'}
-						teamTwo={'Group B 2'}
+						teamTwo={<Galatasaray />}
 						scoreFirstTeamOne={'?'}
 						scoreSecondTeamOne={'?'}
 						scoreFirstTeamTwo={'?'}
 						scoreSecondTeamTwo={'?'}
 					/>
 					<Square
-						teamOne={'Group B 1'}
+						teamOne={<Sporting />}
 						teamTwo={'Group A 2'}
 						scoreFirstTeamOne={'?'}
 						scoreSecondTeamOne={'?'}
