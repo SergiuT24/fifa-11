1180661e7fb055b07fe7857bdc606ab6f30dc54c
Sergiu Tudos <cambino7777@gmail.com>
2025-02-13 19:58:21 +0200

    update feb 13 19:58

--- src/components/league/4star-teams/Leverkusen.jsx
+++ src/components/league/4star-teams/Leverkusen.jsx
@@ -5,7 +5,7 @@ const Leverkusen = () => {
 		<div>
 			<div className='flex gap-1 items-center'>
 				<img className='w-5 h-5' src='https://static.flashscore.com/res/image/data/dWZYQIPq-WrjrBuU2.png' />
-				<p className='text-xs'>Bayern Leverkusen</p>
+				<p className='text-xs'>B.Leverkusen</p>
 			</div>
 		</div>
 	);
