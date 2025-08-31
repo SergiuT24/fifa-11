2d856699e934b2ceab7836e2be6205c317ee58af
Sergiu <132564486+SergiuT24@users.noreply.github.com>
2025-08-31 22:11:53 +0300

    Update UCLTableResult.jsx

--- src/data/season151x1/UCLTableResult.jsx
+++ src/data/season151x1/UCLTableResult.jsx
@@ -1,27 +1,18 @@
-import Juventus from '../../components/league/teams/Juventus';
-import Arsenal from '../../components/league/teams/Arsenal';
 import ACMilan from '../../components/league/teams/AcMilan';
-import ManchesterCity from '../../components/league/teams/ManchesterCity';
-import ManchesterUnited from '../../components/league/teams/ManchesterUnited';
 import RealMadrid from '../../components/league/teams/RealMadrid';
-import Barcelona from '../../components/league/teams/Barcelona';
 import Lyon from '../../components/league/teams/Lyon';
-import Liverpool from '../../components/league/teams/Liverpool';
-import Tottenham from '../../components/league/teams/Tottenham';
-import Inter from '../../components/league/teams/Inter';
-import Chelsea from '../../components/league/teams/Chelsea';
 import AtleticoMadrid from '../../components/league/teams/AtleticoMadrid';
 
 
 export const initialTeams = [
-	{ id: 'RealMadrid', name: <RealMadrid/>, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'AtlecticoMadrid', name: <AtlecticoMadrid/>, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'RealMadrid', name: <RealMadrid />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'AtlecticoMadrid', name: <AtlecticoMadrid />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Place2', name: 'Place2', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
 export const additionalTeams = [
-	{ id: 'ACMilan', name: <ACMilan/>, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'Lyon', name: <Lyon/>, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'ACMilan', name: <ACMilan />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Lyon', name: <Lyon />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Place1', name: 'Place1', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
