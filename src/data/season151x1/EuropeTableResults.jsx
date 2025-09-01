5247360d35dbead34ccd340e76f393510a11dd40
Sergiu <132564486+SergiuT24@users.noreply.github.com>
2025-09-01 23:09:27 +0300

    Update EuropeTableResults.jsx

--- src/data/season151x1/EuropeTableResults.jsx
+++ src/data/season151x1/EuropeTableResults.jsx
@@ -2,47 +2,48 @@ import ManchesterCity from '../../components/league/teams/ManchesterCity';
 import Arsenal from '../../components/league/teams/Arsenal';
 import Barcelona from '../../components/league/teams/Barcelona';
 import Juventus from '../../components/league/teams/Juventus';
-
+import Tottenham from '../../components/league/teams/Tottenham';
+import Juventus from '../../components/league/teams/Juventus';
 
 
 export const initialTeams = [
 	{ id: 'ManchesterCity', name: <ManchesterCity />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'Place 4', name: 'Place 4', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Chelsea', name: <Chelsea /> />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Juventus', name: <Juventus />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 ];
 
 export const additionalTeams = [
 	{ id: 'Arsenal', name: <Arsenal />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
-	{ id: 'Place 3', name: 'Place 3', mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
+	{ id: 'Tottenham', name: <Tottenham />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Barcelona', name: <Barcelona />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 
 ];
 
 export const matchResults = [];
 
-const fetchResults = async () => {
-	console.log('Fetching results...');
-	try {
-		const response = await fetch(`https://match-results-bucket.s3.eu-central-1.amazonaws.com/matchResults.json?timestamp=${Date.now()}`);
-		const data = await response.json();
+// const fetchResults = async () => {
+// 	console.log('Fetching results...');
+// 	try {
+// 		const response = await fetch(`https://match-results-bucket.s3.eu-central-1.amazonaws.com/matchResults.json?timestamp=${Date.now()}`);
+// 		const data = await response.json();
 
-		matchResults.length = 0;
-		data.forEach(item => {
-			matchResults.push({
-				id: item.id,
-				teamOne: item.teamOne,
-				scoreOne: item.scoreOne,
-				teamTwo: item.teamTwo,
-				scoreTwo: item.scoreTwo
-			});
-		});
+// 		matchResults.length = 0;
+// 		data.forEach(item => {
+// 			matchResults.push({
+// 				id: item.id,
+// 				teamOne: item.teamOne,
+// 				scoreOne: item.scoreOne,
+// 				teamTwo: item.teamTwo,
+// 				scoreTwo: item.scoreTwo
+// 			});
+// 		});
 
-		return matchResults;
-	} catch (error) {
-		console.error('Error fetching match results:', error);
-		return [];
-	}
-};
+// 		return matchResults;
+// 	} catch (error) {
+// 		console.error('Error fetching match results:', error);
+// 		return [];
+// 	}
+// };
 
-fetchResults().then(() => {
-});
+// fetchResults().then(() => {
+// });
