eb4bce8491bbe4e6e4e735642fd196bf0a1ecd15
Sergiu <132564486+SergiuT24@users.noreply.github.com>
2025-09-04 19:15:45 +0300

    Update ProLeagueTableResults.jsx

--- src/data/season161x1/ProLeagueTableResults.jsx
+++ src/data/season161x1/ProLeagueTableResults.jsx
@@ -56,4 +56,6 @@ export const matchResults = [
 	{ id: '10', teamOne: 'BayernMunich', scoreOne: 2, teamTwo: 'ManchesterUnited', scoreTwo: 1 },
 	{ id: '10', teamOne: 'Barcelona', scoreOne: 3, teamTwo: 'Sevilla', scoreTwo: 4 },
 	{ id: '9', teamOne: 'Sevilla', scoreOne: 6, teamTwo: 'Barcelona', scoreTwo: 3 },
+	{ id: '10', teamOne: 'AtleticoMadrid', scoreOne: 3, teamTwo: 'ManchesterCity', scoreTwo: 1 },
+	{ id: '9', teamOne: 'ManchesterCity', scoreOne: 4, teamTwo: 'AtleticoMadrid', scoreTwo: 1 },
 ]
