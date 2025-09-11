f21a48488cf3b3e22992288153b17eaf9a9bcd50
Sergiu <132564486+SergiuT24@users.noreply.github.com>
2025-09-11 10:20:13 +0300

    Update ProLeagueTableResults.jsx

--- src/data/seasonSix2x2/ProLeagueTableResults.jsx
+++ src/data/seasonSix2x2/ProLeagueTableResults.jsx
@@ -1,4 +1,3 @@
-import Inter from '../../components/league/teams/Inter';
 import Liverpool from '../../components/league/teams/Liverpool';
 import Barcelona from '../../components/league/teams/Barcelona';
 import ManchesterCity from '../../components/league/teams/ManchesterCity';
@@ -7,7 +6,6 @@ import BayernMunich from '../../components/league/teams/BayernMunich';
 import ACMilan from '../../components/league/teams/AcMilan';
 
 export const initialTeams = [
-	{ id: 'Inter', name: <Inter />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Liverpool', name: <Liverpool />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'Barcelona', name: <Barcelona />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
 	{ id: 'ManchesterCity', name: <ManchesterCity />, mp: 0, w: 0, d: 0, l: 0, g: 0, gc: 0, pts: 0, form: [] },
@@ -17,8 +15,17 @@ export const initialTeams = [
 ];
 
 export const matchResults = [
-	{ id: '1', teamOne: 'Barcelona', scoreOne: 2, teamTwo: 'Inter', scoreTwo: 0 },
-	{ id: '1', teamOne: 'Inter', scoreOne: 2, teamTwo: 'Barcelona', scoreTwo: 1 },
-	{ id: '1', teamOne: 'ManchesterUnited', scoreOne: 8, teamTwo: 'Inter', scoreTwo: 0 },
-	{ id: '1', teamOne: 'Inter', scoreOne: 2, teamTwo: 'ManchesterUnited', scoreTwo: 8 },
+	{ id: '2', teamOne: 'ManchesterCity', scoreOne: 2, teamTwo: 'Liverpool', scoreTwo: 4 },
+	{ id: '2', teamOne: 'Liverpool', scoreOne: 2, teamTwo: 'ManchesterCity', scoreTwo: 3 },
+	{ id: '2', teamOne: 'BayernMunich', scoreOne: 2, teamTwo: 'Liverpool', scoreTwo: 2 },
+	{ id: '2', teamOne: 'Liverpool', scoreOne: 3, teamTwo: 'BayernMunich', scoreTwo: 2 },
+	{ id: '2', teamOne: 'ManchesterUnited', scoreOne: 3, teamTwo: 'ACMilan', scoreTwo: 0 },
+	{ id: '2', teamOne: 'ACMilan', scoreOne: 0, teamTwo: 'ManchesterUnited', scoreTwo: 3 },
+	{ id: '2', teamOne: 'ManchesterUnited', scoreOne: 5, teamTwo: 'Liverpool', scoreTwo: 1 },
+	{ id: '2', teamOne: 'Liverpool', scoreOne: 2, teamTwo: 'ManchesterUnited', scoreTwo: 6 },
+	{ id: '2', teamOne: 'BayernMunich', scoreOne: 2, teamTwo: 'ACMilan', scoreTwo: 0 },
+	{ id: '2', teamOne: 'ACMilan', scoreOne: 3, teamTwo: 'BayernMunich', scoreTwo: 1 },
+	{ id: '2', teamOne: 'ManchesterUnited', scoreOne: 7, teamTwo: 'ManchesterCity', scoreTwo: 2 },
+	{ id: '2', teamOne: 'ManchesterCity', scoreOne: 5, teamTwo: 'ManchesterUnited', scoreTwo: 3 },
 ];
+
